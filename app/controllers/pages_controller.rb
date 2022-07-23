2df30c5289bacd005f042157ffc40d59ffb40be2
afeez oloyede <oloyedeafeez@363photography.org>
2022-07-23 13:16:46 -0700

    Changed the arrangement of the button on the art show page modal

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @artist = Artist.find(current_artist.id)
     @art = Art.new
+    @art4 = Art.all.sample(3)
     if params[:query].present?
       @arts = Art.search_by_description_artist(params[:query])
     else
