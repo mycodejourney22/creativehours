3f13dd6b8815e3cdce102f436af4a6cb59d7cb30
afeez oloyede <oloyedeafeez@363photography.org>
2022-08-12 09:43:08 -0700

    remove targets from stimulus file of forms and use sibling element

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,27 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="form"
 export default class extends Controller {
-  static targets = ["customLabelInput", "customPasswordField", "customLabelPassword","customInputField"]
 
   connect() {
 
   }
 
-  checkInput(){
-    if (this.customInputFieldTarget.value.length > 0){
-      this.customLabelInputTarget.classList.remove("opacity-0")
-    }
-    else {
-      this.customLabelInputTarget.classList.add("opacity-0")
-    }
-  }
-
-  checkPassword(){
-    if (this.customPasswordFieldTarget.value.length > 0){
-      this.customLabelPasswordTarget.classList.remove("opacity-0")
-    }
-    else {
-      this.customLabelPasswordTarget.classList.add("opacity-0")
-    }
-  }
+  inputCheck(event){
+    const inputField = event.currentTarget
+    const labelField = inputField.previousSibling
+   if (inputField.value.length > 0){
+     labelField.classList.remove("opacity-0")
+   }
+   else {
+     labelField.classList.add("opacity-0")
+   }
+ }
 }
