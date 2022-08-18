cba068aec5fedaae93e9f7f6cf8a9785a66695a7
afeez oloyede <oloyedeafeez@363photography.org>
2022-08-18 00:55:05 -0700

    update follower count with ajax

--- app/javascript/controllers/follow_controller.js
+++ app/javascript/controllers/follow_controller.js
@@ -2,22 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="follow"
 export default class extends Controller {
+  static targets = ["followNumber"]
   connect() {
     this.csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content")
   }
 
   changeStatus(event){
-    this.formElement = this.element.firstElementChild
+    this.formElement = event.currentTarget.firstElementChild
+    this.parentElement = event.currentTarget
     event.preventDefault()
 
     fetch(this.formElement.action, {
       method: "POST",
       headers: { "Accept": "application/json", "X-CSRF-Token": this.csrfToken },
-      body: new FormData(this.formTarget)
+      body: new FormData(this.formElement)
     })
       .then(response => response.json())
       .then((data) => {
-        this.formElement.outerHTML = data.form
+        this.parentElement.outerHTML = data.form
+        this.followNumberTarget.outerHTML = data.follow_count
       })
 
 
--- app/views/artists/follow.json.jbuilder
+++ app/views/artists/follow.json.jbuilder
@@ -1,1 +1,2 @@
 json.form render(partial: "follow", formats: :html, locals: {artist: @artist})
+json.follow_count render(partial: "followercount", formats: :html, locals: {artist: @artist})
