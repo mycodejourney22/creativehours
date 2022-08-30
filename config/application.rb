e222a3f7867afb21d626b9f13d536d2ef95dbcfb
afeez oloyede <oloyedeafeez@363photography.org>
2022-08-29 19:37:46 -0700

    prevent sass from compressor

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Creativehours
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.assets.paths << "#{Rails.root}/app/assets/videos"
+
+    config.assets.css_compressor = nil
   end
 
 end
